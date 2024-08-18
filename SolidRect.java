e87eba51a8d51678bdd58a52f67687d848acf495
asavarikadam1774 <asavarikadam942151@gmail.com>
2024-08-18 23:49:46 +0530

    Add files via upload

---
+++ SolidRect.java
@@ -0,0 +1,17 @@
+
+class SolidRect{
+    public static void main(String[] args) {
+        int i,j;
+        int n=4;
+        int m=5;
+        for(i=1;i<=n;i++)
+        {
+            for(j=1;j<=m;j++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+    }
+}
